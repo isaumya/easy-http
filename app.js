42c051bec541122f27890ad36fee708491bac8f4
Saumya Majumder <saumya0305@gmail.com>
2018-06-11 13:40:25 +0530

    Completed the easy-http-es5 js file and tests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = new EasyHTTP();
 
+// Get Posts - GET method
 http.get('https://jsonplaceholder.typicode.com/posts', function(err, response){
 	if(err) {
 		document.getElementById('response').innerHTML = `<pre>${err}</pre>`;
@@ -7,3 +8,52 @@ http.get('https://jsonplaceholder.typicode.com/posts', function(err, response){
 		document.getElementById('response').innerHTML = `<pre>${response}</pre>`;
 	}
 });
+
+/*const article = [
+	{ 
+		userId: 27,
+		title: "This is a post about something, maybe sleep",
+		body: "I don't know why I'm feeling so damn sleepy that even though it is raining outside I can barely look at that." 
+	},
+	{
+		userId: 24,
+		title: "Article Two",
+		body: "This is the body of article two"
+	}
+];*/
+
+const article = {
+	userId: 27,
+	title: "This is a post about something, maybe sleep",
+	body: "I don't know why I'm feeling so damn sleepy that even though it is raining outside I can barely look at that."
+};
+
+// Create post - POST Method
+// http.post('https://jsonplaceholder.typicode.com/posts', article, function(err, response) {
+// 	if(err) {
+// 		document.getElementById('response').innerHTML = `<pre>${err}</pre>`;
+// 	} else {
+// 		document.getElementById('response').innerHTML = `<pre>${response}</pre>`;
+// 	}
+// });
+
+// Update Post - PUT Method
+// Lets update the post with post id 1 with out above article JSON
+// http.put('https://jsonplaceholder.typicode.com/posts/1', article, function(err, response) {
+// 	if(err) {
+// 		document.getElementById('response').innerHTML = `<pre>${err}</pre>`;
+// 	} else {
+// 		document.getElementById('response').innerHTML = `<pre>${response}</pre>`;
+
+// 	}
+// });
+
+// Delete Post - DELETE Method
+// Deleting the post with ID 5
+// http.delete('https://jsonplaceholder.typicode.com/posts/5', function(err, response) {
+// 	if(err) {
+// 		document.getElementById('response').innerHTML = `<pre>${err}</pre>`;
+// 	} else {
+// 		document.getElementById('response').innerHTML = `<pre>${response}</pre>`;
+// 	}
+// });
--- easy-http.js
+++ easy-http.js
@@ -25,16 +25,54 @@ EasyHTTP.prototype.get = function(url, callback) {
 }
 
 // Make an HTTP POST Request
-EasyHTTP.prototype.post = function() {
+EasyHTTP.prototype.post = function(url, data, callback) {
+	// Open the api url
+	this.http.open('POST', url, true);
+	// Set the content type header
+	this.http.setRequestHeader('Content-type', 'application/json');
 
+	// Do the onload stuffs
+	const self = this;
+	this.http.onload = function() {
+		callback(null, self.http.responseText);
+	}
+
+	// Send the data to the server
+	this.http.send(JSON.stringify(data));
 }
 
 // Make an HTTP PUT Request
-EasyHTTP.prototype.put = function() {
+EasyHTTP.prototype.put = function(url, data, callback) {
+	// Lets open the request
+	this.http.open('PUT', url, true);
+	// Set the Content-type header
+	this.http.setRequestHeader('Content-type', 'application/json');
 
+	// do onload stuffs
+	const self = this;
+	this.http.onload = function() {
+		callback(null, self.http.responseText);
+	}
+
+	// Send the data to server
+	this.http.send(JSON.stringify(data));
 }
 
 // Make an HTTP DELETE Request
-EasyHTTP.prototype.delete = function() {
-
+EasyHTTP.prototype.delete = function(url, callback) {
+	// Let's open the API url
+	this.http.open('DELETE', url, true);
+	// Do the onload stuffs
+	const self = this;
+	let err = null;
+	this.http.onload = function() {
+		if(self.http.status === 200) {
+			callback(err, 'The post has been DELETED successfully.');
+		} else {
+			err = `Error: ${self.http.status}`;
+			callback(err, null);
+		}
+	}
+	// Send response to the server
+	this.http.send();
 }
